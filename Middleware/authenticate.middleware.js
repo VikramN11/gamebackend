3eb51178e105262a551b597d093b172c9d912bd9
Vikram Navik <vikramnavik98@gmail.com>
2023-03-04 18:55:10 +0530

    auth

--- Middleware/authenticate.middleware.js
+++ Middleware/authenticate.middleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const authentication = (req, res, next) =>{
-    const token = req.headers.authorization;
+    const token = req.headers.Authorization;
     jwt.verify(token, 'chidiyaudd', (err, decoded)=>{
         if(decoded){
             // req.body.user=decoded.userID;
